4dd04e18f3382136a07474b68c6aece9392e8df3
steven noppe <82015720+StevenNoppeVetex@users.noreply.github.com>
2021-08-20 14:22:21 +0200

    Revert "Knoppen toegevoegd op formulier "ingave doek""
    This reverts commit 4b09d0430cbb9b10e3e4f842cc4584a761ef9920.

--- Dashboard_Villars_dotnet/frmDoek.Designer.cs
+++ Dashboard_Villars_dotnet/frmDoek.Designer.cs
@@ -33,19 +33,6 @@ namespace Dashboard_Villars_dotnet
             this.btnAnnuleren = new System.Windows.Forms.Button();
             this.btnOK = new System.Windows.Forms.Button();
             this.txtIngaveDoek = new System.Windows.Forms.TextBox();
-            this.btnC = new System.Windows.Forms.Button();
-            this.btn0 = new System.Windows.Forms.Button();
-            this.btn9 = new System.Windows.Forms.Button();
-            this.btn8 = new System.Windows.Forms.Button();
-            this.btn7 = new System.Windows.Forms.Button();
-            this.btn6 = new System.Windows.Forms.Button();
-            this.btn5 = new System.Windows.Forms.Button();
-            this.btn4 = new System.Windows.Forms.Button();
-            this.btn3 = new System.Windows.Forms.Button();
-            this.btn2 = new System.Windows.Forms.Button();
-            this.btn1 = new System.Windows.Forms.Button();
-            this.btnE = new System.Windows.Forms.Button();
-            this.btnF = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnWis
@@ -89,167 +76,11 @@ namespace Dashboard_Villars_dotnet
             this.txtIngaveDoek.Size = new System.Drawing.Size(486, 44);
             this.txtIngaveDoek.TabIndex = 0;
             // 
-            // btnC
-            // 
-            this.btnC.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnC.Location = new System.Drawing.Point(90, 60);
-            this.btnC.Name = "btnC";
-            this.btnC.Size = new System.Drawing.Size(60, 60);
-            this.btnC.TabIndex = 22;
-            this.btnC.Text = "C";
-            this.btnC.UseVisualStyleBackColor = true;
-            this.btnC.Click += new System.EventHandler(this.btnC_Click);
-            // 
-            // btn0
-            // 
-            this.btn0.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn0.Location = new System.Drawing.Point(156, 258);
-            this.btn0.Name = "btn0";
-            this.btn0.Size = new System.Drawing.Size(60, 60);
-            this.btn0.TabIndex = 21;
-            this.btn0.Text = "0";
-            this.btn0.UseVisualStyleBackColor = true;
-            this.btn0.Click += new System.EventHandler(this.btn0_Click);
-            // 
-            // btn9
-            // 
-            this.btn9.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn9.Location = new System.Drawing.Point(288, 192);
-            this.btn9.Name = "btn9";
-            this.btn9.Size = new System.Drawing.Size(60, 60);
-            this.btn9.TabIndex = 20;
-            this.btn9.Text = "9";
-            this.btn9.UseVisualStyleBackColor = true;
-            this.btn9.Click += new System.EventHandler(this.btn9_Click);
-            // 
-            // btn8
-            // 
-            this.btn8.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn8.Location = new System.Drawing.Point(222, 192);
-            this.btn8.Name = "btn8";
-            this.btn8.Size = new System.Drawing.Size(60, 60);
-            this.btn8.TabIndex = 19;
-            this.btn8.Text = "8";
-            this.btn8.UseVisualStyleBackColor = true;
-            this.btn8.Click += new System.EventHandler(this.btn8_Click);
-            // 
-            // btn7
-            // 
-            this.btn7.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn7.Location = new System.Drawing.Point(156, 192);
-            this.btn7.Name = "btn7";
-            this.btn7.Size = new System.Drawing.Size(60, 60);
-            this.btn7.TabIndex = 18;
-            this.btn7.Text = "7";
-            this.btn7.UseVisualStyleBackColor = true;
-            this.btn7.Click += new System.EventHandler(this.btn7_Click);
-            // 
-            // btn6
-            // 
-            this.btn6.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn6.Location = new System.Drawing.Point(288, 126);
-            this.btn6.Name = "btn6";
-            this.btn6.Size = new System.Drawing.Size(60, 60);
-            this.btn6.TabIndex = 17;
-            this.btn6.Text = "6";
-            this.btn6.UseVisualStyleBackColor = true;
-            this.btn6.Click += new System.EventHandler(this.btn6_Click);
-            // 
-            // btn5
-            // 
-            this.btn5.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn5.Location = new System.Drawing.Point(222, 126);
-            this.btn5.Name = "btn5";
-            this.btn5.Size = new System.Drawing.Size(60, 60);
-            this.btn5.TabIndex = 16;
-            this.btn5.Text = "5";
-            this.btn5.UseVisualStyleBackColor = true;
-            this.btn5.Click += new System.EventHandler(this.btn5_Click);
-            // 
-            // btn4
-            // 
-            this.btn4.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn4.Location = new System.Drawing.Point(156, 126);
-            this.btn4.Name = "btn4";
-            this.btn4.Size = new System.Drawing.Size(60, 60);
-            this.btn4.TabIndex = 15;
-            this.btn4.Text = "4";
-            this.btn4.UseVisualStyleBackColor = true;
-            this.btn4.Click += new System.EventHandler(this.btn4_Click);
-            // 
-            // btn3
-            // 
-            this.btn3.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn3.Location = new System.Drawing.Point(288, 60);
-            this.btn3.Name = "btn3";
-            this.btn3.Size = new System.Drawing.Size(60, 60);
-            this.btn3.TabIndex = 14;
-            this.btn3.Text = "3";
-            this.btn3.UseVisualStyleBackColor = true;
-            this.btn3.Click += new System.EventHandler(this.btn3_Click);
-            // 
-            // btn2
-            // 
-            this.btn2.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn2.Location = new System.Drawing.Point(222, 60);
-            this.btn2.Name = "btn2";
-            this.btn2.Size = new System.Drawing.Size(60, 60);
-            this.btn2.TabIndex = 13;
-            this.btn2.Text = "2";
-            this.btn2.UseVisualStyleBackColor = true;
-            this.btn2.Click += new System.EventHandler(this.btn2_Click);
-            // 
-            // btn1
-            // 
-            this.btn1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn1.Location = new System.Drawing.Point(156, 60);
-            this.btn1.Name = "btn1";
-            this.btn1.Size = new System.Drawing.Size(60, 60);
-            this.btn1.TabIndex = 12;
-            this.btn1.Text = "1";
-            this.btn1.UseVisualStyleBackColor = true;
-            this.btn1.Click += new System.EventHandler(this.btn1_Click);
-            // 
-            // btnE
-            // 
-            this.btnE.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnE.Location = new System.Drawing.Point(90, 126);
-            this.btnE.Name = "btnE";
-            this.btnE.Size = new System.Drawing.Size(60, 60);
-            this.btnE.TabIndex = 23;
-            this.btnE.Text = "E";
-            this.btnE.UseVisualStyleBackColor = true;
-            this.btnE.Click += new System.EventHandler(this.btnE_Click);
-            // 
-            // btnF
-            // 
-            this.btnF.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnF.Location = new System.Drawing.Point(90, 192);
-            this.btnF.Name = "btnF";
-            this.btnF.Size = new System.Drawing.Size(60, 60);
-            this.btnF.TabIndex = 24;
-            this.btnF.Text = "F";
-            this.btnF.UseVisualStyleBackColor = true;
-            this.btnF.Click += new System.EventHandler(this.btnF_Click);
-            // 
             // frmDoek
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(505, 457);
-            this.Controls.Add(this.btnF);
-            this.Controls.Add(this.btnE);
-            this.Controls.Add(this.btnC);
-            this.Controls.Add(this.btn0);
-            this.Controls.Add(this.btn9);
-            this.Controls.Add(this.btn8);
-            this.Controls.Add(this.btn7);
-            this.Controls.Add(this.btn6);
-            this.Controls.Add(this.btn5);
-            this.Controls.Add(this.btn4);
-            this.Controls.Add(this.btn3);
-            this.Controls.Add(this.btn2);
-            this.Controls.Add(this.btn1);
             this.Controls.Add(this.btnWis);
             this.Controls.Add(this.btnAnnuleren);
             this.Controls.Add(this.btnOK);
@@ -269,18 +100,5 @@ namespace Dashboard_Villars_dotnet
         private System.Windows.Forms.Button btnAnnuleren;
         private System.Windows.Forms.Button btnOK;
         private System.Windows.Forms.TextBox txtIngaveDoek;
-        private System.Windows.Forms.Button btnC;
-        private System.Windows.Forms.Button btn0;
-        private System.Windows.Forms.Button btn9;
-        private System.Windows.Forms.Button btn8;
-        private System.Windows.Forms.Button btn7;
-        private System.Windows.Forms.Button btn6;
-        private System.Windows.Forms.Button btn5;
-        private System.Windows.Forms.Button btn4;
-        private System.Windows.Forms.Button btn3;
-        private System.Windows.Forms.Button btn2;
-        private System.Windows.Forms.Button btn1;
-        private System.Windows.Forms.Button btnE;
-        private System.Windows.Forms.Button btnF;
     }
 }
